5bff62542feb532eb4d206bfe93e5da212137d94
Håkon <haakonfs@hotmail.com>
2022-11-09 00:20:05 +0100

    added table name for plant

--- react-java-mysql/Plants/src/main/java/no/ntnu/Plants/entity/Plant.java
+++ react-java-mysql/Plants/src/main/java/no/ntnu/Plants/entity/Plant.java
@@ -2,8 +2,10 @@ package no.ntnu.Plants.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "plant")
 public class Plant {
     
     @Id
