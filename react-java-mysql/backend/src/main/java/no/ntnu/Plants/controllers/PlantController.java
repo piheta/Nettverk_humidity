9bb66fbbf7d7bb01c27f08a2f572c2eb0a965238
Håkon Sætre <hakonz3@dhcp-10-22-194-1.wlan.ntnu.no>
2022-12-06 11:34:48 +0100

    redid get all plant method and crossOrigin

--- react-java-mysql/backend/src/main/java/no/ntnu/Plants/controllers/PlantController.java
+++ react-java-mysql/backend/src/main/java/no/ntnu/Plants/controllers/PlantController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
